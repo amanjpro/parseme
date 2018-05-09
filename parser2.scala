b462cd3848a16afb2660380ad12cc641d39d17ee
amanjpro <amanj.sherwany@adgear.com>
2018-05-09 13:09:12 -0400

    First complicated attempt, s3

--- parser2.scala
+++ parser2.scala
@@ -2,3 +2,13 @@
 type Parser = String => Option[String]
 
 def ok: Parser = (str: String) => Some(str)
+
+def char(ch: Char): Parser = (str: String) => {
+  if(! str.isEmpty && str.charAt(0) == ch) {
+    Some(str.tail)
+  } else None
+}
+
+// Usage examples
+val a = char('a')
+val b = char('b')
